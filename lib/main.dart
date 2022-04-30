8f8aaded294a13a84974263a3eb794999ff2e68c
IslamWahied <56431823+islamwahied@users.noreply.github.com>
2022-05-01 01:20:58 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+// @dart=2.9
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+import 'package:fast_chat/layout/social_app/cubit/cubit.dart';
+import 'package:fast_chat/layout/social_app/social_layout.dart';
+
+import 'package:fast_chat/modules/social_app/social_login/social_login_screen.dart';
+import 'package:fast_chat/shared/bloc_observer.dart';
+import 'package:fast_chat/shared/components/constants.dart';
+import 'package:fast_chat/shared/cubit/cubit.dart';
+import 'package:fast_chat/shared/cubit/states.dart';
+import 'package:fast_chat/shared/network/local/cache_helper.dart';
+
+import 'package:fast_chat/shared/styles/themes.dart';
+
+void main() async {
+  // بيتأكد ان كل حاجه هنا في الميثود خلصت و بعدين يتفح الابلكيشن
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  // Bloc.observer = MyBlocObserver();
+
+  await CacheHelper.init();
+
+  bool isDark = CacheHelper.getData(key: 'isDark');
+
+  Widget widget;
+
+  //bool onBoarding = CacheHelper.getData(key: 'onBoarding');
+  //token = CacheHelper.getData(key: 'token');
+
+  uId = CacheHelper.getData(key: 'uId');
+
+  // if(onBoarding != null)
+  // {
+  //   if(token != null) widget = ShopLayout();
+  //   else widget = ShopLoginScreen();
+  // } else
+  //   {
+  //     widget = OnBoardingScreen();
+  //   }
+
+  if(uId != null)
+  {
+    widget = SocialLayout();
+  } else
+  {
+    widget = SocialLoginScreen();
+  }
+
+  runApp(MyApp(
+    isDark: isDark,
+    startWidget: widget,
+  ));
+}
+
+// Stateless
+// Stateful
+
+// class MyApp
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+  final bool isDark;
+  final Widget startWidget;
+
+  MyApp({
+    this.isDark,
+    this.startWidget,
+  });
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MultiBlocProvider(
+      providers: [
+
+        BlocProvider(
+          create: (BuildContext context) => AppCubit()
+            ..changeAppMode(
+              fromShared: isDark,
+            ),
+        ),
+
+        BlocProvider(
+          create: (BuildContext context) => SocialCubit()..getUserData(),
+        ),
+      ],
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: ThemeMode.light,
+            home: startWidget,
+          );
+        },
+      ),
+    );
+  }
+}
+
+// ./gradlew signingReport
---
+++ lib/modules/social_app/chats/chats_screen.dart
@@ -0,0 +1,65 @@
+// @dart=2.9
+import 'package:conditional_builder/conditional_builder.dart';
+import 'package:fast_chat/layout/social_app/cubit/cubit.dart';
+import 'package:fast_chat/layout/social_app/cubit/states.dart';
+import 'package:fast_chat/models/social_app/social_user_model.dart';
+import 'package:fast_chat/modules/social_app/chat_details/chat_details_screen.dart';
+import 'package:fast_chat/shared/components/components.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class ChatsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SocialCubit, SocialStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return ConditionalBuilder(
+          condition: SocialCubit.get(context).users.length > 0,
+          builder: (context) => ListView.separated(
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) =>
+                buildChatItem(SocialCubit.get(context).users[index], context),
+            separatorBuilder: (context, index) => myDivider(),
+            itemCount: SocialCubit.get(context).users.length,
+          ),
+          fallback: (context) => Center(child: CircularProgressIndicator()),
+        );
+      },
+    );
+  }
+
+  Widget buildChatItem(SocialUserModel model, context) => InkWell(
+        onTap: () {
+          navigateTo(
+            context,
+            ChatDetailsScreen(
+              userModel: model,
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 25.0,
+                backgroundImage: NetworkImage(
+                  '${model.image}',
+                ),
+              ),
+              SizedBox(
+                width: 15.0,
+              ),
+              Text(
+                '${model.name}',
+                style: TextStyle(
+                  height: 1.4,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+}
