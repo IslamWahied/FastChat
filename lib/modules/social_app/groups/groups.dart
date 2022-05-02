8f2928fa02824fe05d5c9b77c7eb06829bcc2d24
Youssef <hala.waheedch.20102@gmail.com>
2022-05-02 05:04:25 +0200

    youssef 8

--- lib/modules/social_app/groups/groups.dart
+++ lib/modules/social_app/groups/groups.dart
@@ -20,15 +20,207 @@ class GroupsScreen extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         var cubit=SocialCubit.get(context);
-        return ConditionalBuilder(
-          condition: cubit.listUsers.isNotEmpty && cubit.listUsers.where((element) => element.uId != uId).toList().isNotEmpty,
-          builder: (context) => ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) => buildChatItem( cubit.listUsers.where((element) => element.uId != uId).toList()[index], context),
-            separatorBuilder: (context, index) => myDivider(),
-            itemCount: SocialCubit.get(context).listUsers.where((element) => element.uId != uId).toList().length,
-          ),
-          fallback: (context) => const Center(child: CircularProgressIndicator()),
+        return Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: (){
+
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return StatefulBuilder(
+                            builder: (context,setState){
+                              return AlertDialog(
+                                content: SizedBox(
+                                  height: MediaQuery.of(context).size.height * 0.2,
+                                  width: MediaQuery.of(context).size.width * 0.4,
+                                  child: SingleChildScrollView(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              'Sort by:',
+                                              style: TextStyle(
+                                                  color: Colors.green,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                InkWell(
+                                                  onTap: () {
+                                                    setState((){
+                                                    //  cubit.changeUpperSort();
+
+                                                    });
+                                                  },
+                                                  child: CircleAvatar(
+                                                    radius: 15,
+                                                    child: const Icon(
+                                                      Icons.arrow_downward,
+                                                      color: Colors.white,
+                                                      size: 17,
+                                                    ),
+
+                                                  //  cubit.LowerColor,
+                                                  ),
+                                                ),
+                                                SizedBox(
+                                                  width: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                      0.03,
+                                                ),
+                                                InkWell(
+                                                  onTap: () {
+                                                    setState((){
+                                                     // cubit.changeLowerSort();
+
+                                                    });
+                                                  },
+                                                  child: CircleAvatar(
+                                                    radius: 15,
+                                                    child: const Icon(
+                                                      Icons.arrow_upward,
+                                                      color: Colors.white,
+                                                      size: 17,
+                                                    ),
+
+                                                   // cubit.upperColor,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                        const Divider(
+                                          thickness: 1,
+                                        ),
+                                        SizedBox(
+                                          height:
+                                          MediaQuery.of(context).size.height /
+                                              33,
+                                        ),
+                                        InkWell(
+                                          onTap: () {
+                                            //cubit.sortByDate(context);
+                                          },
+                                          child: Row(
+                                            mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Sort by date',
+                                                style: TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.w500),
+                                              ),
+                                              const Icon(Icons.sort)
+                                            ],
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height:
+                                          MediaQuery.of(context).size.height /
+                                              33,
+                                        ),
+                                        InkWell(
+                                          onTap: () {
+                                            //cubit.sortByNum(context);
+                                          },
+                                          child: Row(
+                                            mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text(
+                                                'Sort by case number',
+                                                style: TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.w500),
+                                              ),
+                                              const Icon(Icons.sort)
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                elevation: 24,
+                                backgroundColor: Colors.blueGrey[50],
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                    BorderRadius.all(Radius.circular(25.0))),
+                              );
+                            },
+
+                          );
+                        },
+                      );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.blue),
+                        child: Row(
+                          children: const [
+                            Text('New Group',style: TextStyle(color: Colors.white),),
+                            SizedBox(width: 5,),
+                            Icon(Icons.add,color: Colors.white,),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ConditionalBuilder(
+                condition: cubit.listUsers.isNotEmpty && cubit.listUsers.where((element) => element.uId != uId).toList().isNotEmpty,
+                builder: (context) => ListView.separated(
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) => buildChatItem( cubit.listUsers.where((element) => element.uId != uId).toList()[index], context),
+                  separatorBuilder: (context, index) => myDivider(),
+                  itemCount: SocialCubit.get(context).listUsers.where((element) => element.uId != uId).toList().length,
+                ),
+                fallback: (context) => const Center(child: CircularProgressIndicator()),
+              ),
+            ),
+          ],
         );
       },
     );
